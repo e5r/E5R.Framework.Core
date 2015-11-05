b78133a4577515ddcd5cf7f4ff2189c0582990d2
Erlimar Silva Campos <erlimar@gmail.com>
2015-11-05 04:04:27 -0200

    Make command implemented

--- README.md
+++ README.md
@@ -6,15 +6,14 @@ Esqueleto base para projetos **E5R Development Team**.
 ## Pré-requisitos
 
 * NodeJS >=4.2
-* NPM >=2.14
-* Gulp >= 3.9.0
+* NPM >=3.3
 
 ## Ajuda
 
 O comando abaixo exibe informações que ajudam você a entender o mecanismo de construção.
 
 ```
-shell> gulp
+$> make
 ```
 
 ## Configurar
@@ -23,7 +22,7 @@ Você pode usar o comando abaixo para configurar suas ferramentas de desenvolvim
 os padrões utilizados no desenvolvimento.
 
 ```
-shell> gulp configure [options]
+$> make configure [options]
 ```
 
 ### options
--- make.cmd
+++ make.cmd
@@ -1,20 +1,17 @@
-@Rem // Copyright (c) E5R Development Team. All rights reserved.
-@Rem // Licensed under the Apache License, Version 2.0. More license information in LICENSE.txt.
-
 @echo off
-
-goto exitSetErrorLevel 
+rem // Copyright (c) E5R Development Team. All rights reserved.
+rem // Licensed under the Apache License, Version 2.0. More license information in LICENSE.txt.
 
 where node 2>nul >nul
 IF %ERRORLEVEL% NEQ 0 (
     echo Require NODEJS executable. Please install node.js.
-    goto error
+    goto :error
 )
 
 where npm 2>nul >nul
 IF %ERRORLEVEL% NEQ 0 (
     echo Require NPM executable. Please install npm.js.
-    goto error
+    goto :error
 )
 
 IF NOT EXIST ".\node_modules\gulp" (
@@ -23,12 +20,12 @@ IF NOT EXIST ".\node_modules\gulp" (
 
 IF NOT EXIST ".\node_modules\gulp" (
     echo Require GULP executable. Please install gulp.js.
-    goto error
+    goto :error
 )
 
 call .\node_modules\.bin\gulp.cmd %* 
 
-goto end
+goto :end
 
 :error
 endlocal
@@ -37,11 +34,9 @@ call :exitSetErrorLevel
 call :exitFromFunction 2>nul
 
 :exitSetErrorLevel
-echo exitSetErrorLevel
 exit /b 1
 
 :exitFromFunction
-echo exitFromFunction
 ()
 
 :end
